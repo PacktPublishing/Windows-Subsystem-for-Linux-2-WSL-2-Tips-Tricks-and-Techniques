60085e27e20a0d6d6ba4fcef0fdf0da666841529
Stuart Leeks <stuart@leeks.net>
2020-06-30 07:16:26 +0100

    Update chapter 2 example commands

--- chapter-02/02-example-commands.sh
+++ chapter-02/02-example-commands.sh
@@ -2,6 +2,12 @@
 echo "This file contains example commands from chapter 2 and is not meant to be executed as a script"
 exit 0
 
+#
+# NOTE: This file contains example commands from chapter 2
+#       Where distro names and user names are used, you will likely need to change based on your system
+#
+
+
 # List distros
 wsl --list
 
@@ -17,10 +23,22 @@ wsl --set-version Ubuntu 2
 # List files in the user home folder in the default distro
 wsl ls ~
 
+# Dump the `/etc/issue` contents from the default distro
+wsl cat /etc/issue
+
 # Dump the `/etc/issue` contents from the `Ubuntu-20.04` distro
 wsl -d Ubuntu-20.04 cat /etc/issue
 
-# Specify the user to run a command as
+# Dump the `/etc/issue` contents from the `Alpine` distro
+wsl -d Alpine cat /etc/issue
+
+# Run `whoami` as the default user
+wsl whoami
+
+# Run `whoami` as `stuart` user
+wsl -u stuart whoami
+
+# Run `whoami` as `root` user
 wsl -u root whoami
 
 # Terminate the `Ubuntu` distro
