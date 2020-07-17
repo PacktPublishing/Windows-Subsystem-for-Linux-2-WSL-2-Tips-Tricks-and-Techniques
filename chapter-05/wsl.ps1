a5bdfcd242ec9a5713cdf006234726d9d02fb53e
Stuart Leeks <stuart@leeks.net>
2020-07-17 15:57:03 +0100

    Fix typo

--- chapter-05/wsl.ps1
+++ chapter-05/wsl.ps1
@@ -6,5 +6,5 @@ param (
 )
 
 Write-Host "Hello from WSL: $Name"
-Write-Host "Current direcory: $PWD"
+Write-Host "Current directory: $PWD"
 Get-EventLog -Log Application | Select-Object -First 10 | Format-Table Index,Source, Message
